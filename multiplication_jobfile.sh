3ae9b92617277169235cc81c42062ec1065d5e95
Rupak Mukherjee <rupakmukherjee01@gmail.com>
2020-09-12 18:09:38 -0400

    Update multiplication_jobfile.sh

--- multiplication_jobfile.sh
+++ multiplication_jobfile.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH --job-name=multiplication_test      # Job name
-#SBATCH --nodes=1                     # Run on a single CPU
-#SBATCH --ntasks-per-node=1           # Run with a single thread
-#SBATCH --time=00:01:00               # Time limit hrs:min:sec
-#SBATCH --partition=picluster         # Name of the partition
+#SBATCH --nodes=1                           # Run on a single CPU
+#SBATCH --ntasks-per-node=1                 # Run with a single thread
+#SBATCH --time=00:01:00                     # Time limit hrs:min:sec
+#SBATCH --partition=picluster               # Name of the partition
 
 cd $SLURM_SUBMIT_DIR
 gfortran multiplication.f95
